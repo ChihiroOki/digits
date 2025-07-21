be5acdb70573210cc0e2dc154f5bd95963d5e25d
Chihiro Oki <oc9@hawaii.edu>
2025-07-20 20:47:43 -1000

    second trial

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -4,6 +4,8 @@ import { loggedInProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import ContactCard from '@/components/ContactCard';
 import { prisma } from '@/lib/prisma';
+import { Contact } from '@prisma/client';
+
 /** Render a list of stuff for the logged in user. */
 const ListPage = async () => {
   // Protect the page, only logged in users can access it.
@@ -15,12 +17,16 @@ const ListPage = async () => {
     } | null,
   );
   const owner = session?.user!.email ? session.user.email : '';
-  const contacts = await prisma.contact.findMany({
+  const contacts: Contact[] = await prisma.contact.findMany({
     where: {
       owner,
     },
   });
-  console.log(contacts);
+  const notes = await prisma.note.findMany({
+    where: {
+      owner,
+    },
+  });
   return (
     <main>
       <Container id="list" fluid className="py-3">
@@ -31,7 +37,7 @@ const ListPage = async () => {
               <Row xs={1} md={2} lg={3} className="g-4">
                 {contacts.map((contact) => (
                   <Col key={contact.firstName + contact.lastName}>
-                    <ContactCard contact={contact} />
+                    <ContactCard contact={contact} notes={notes.filter(note => (note.contactId === contact.id))} />
                   </Col>
                 ))}
               </Row>
--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import { Contact } from '@prisma/client';
+import { Contact, Note } from '@prisma/client';
 import Link from 'next/link';
-import { Card } from 'react-bootstrap';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+import NoteItem from './NoteItem';
+import AddNoteForm from './AddNoteForm';
 
 /* Renders a single Contact. See list/page.tsx. */
-const ContactCard = ({ contact }: { contact : Contact }) => (
+const ContactCard = ({ contact, notes }: { contact : Contact, notes: Note[] }) => (
   <Card className="h-100">
     <Card.Header>
-      <img src={contact.image} width={75} alt={`${contact.firstName} ${contact.lastName}`} />
+      <Image src={contact.image} width={75} alt={`${contact.firstName} ${contact.lastName}`} />
       <Card.Title>
         {contact.firstName}
         &nbsp;
@@ -20,6 +22,10 @@ const ContactCard = ({ contact }: { contact : Contact }) => (
       <Card.Text>
         {contact.description}
       </Card.Text>
+      <ListGroup variant="flush">
+        {notes.map((note) => <NoteItem key={note.id} note={note} />)}
+      </ListGroup>
+      <AddNoteForm contact={contact} />
     </Card.Body>
     <Card.Footer>
       <Link href={`edit/${contact.id}`}>Edit</Link>
