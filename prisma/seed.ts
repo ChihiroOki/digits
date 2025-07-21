670fcd094367f2008456392257b90308c7d498f2
Chihiro Oki <oc9@hawaii.edu>
2025-07-20 15:13:18 -1000

    fix some

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,70 +6,88 @@ const prisma = new PrismaClient();
 
 async function main() {
   console.log('Seeding the database');
-
   const password = await hash('changeme', 10);
-
-  const userPromises = [];
-  for (const account of config.defaultAccounts) {
-    const role = account.role as Role || Role.USER;
+  config.defaultAccounts.forEach(async (account) => {
+    let role: Role = 'USER';
+    if (account.role === 'ADMIN') {
+      role = 'ADMIN';
+    }
     console.log(`  Creating user: ${account.email} with role: ${role}`);
-    userPromises.push(
-      prisma.user.upsert({
-        where: { email: account.email },
-        update: {},
-        create: {
-          email: account.email,
-          password,
-          role,
-        },
-      }),
-    );
-  }
-  await Promise.all(userPromises);
+    await prisma.user.upsert({
+      where: { email: account.email },
+      update: {},
+      create: {
+        email: account.email,
+        password,
+        role,
+      },
+    });
+  });
 
-  const stuffPromises = [];
-  for (const data of config.defaultData) {
-    const condition = data.condition as Condition || Condition.good;
-    console.log(`  Adding stuff: ${JSON.stringify(data)}`);
-    stuffPromises.push(
-      prisma.stuff.upsert({
-        where: { id: config.defaultData.indexOf(data) + 1 },
-        update: {},
-        create: {
+  config.defaultData.forEach(async (data) => {
+    let condition: Condition = 'good';
+    if (data.condition === 'poor') {
+      condition = 'poor';
+    } else if (data.condition === 'excellent') {
+      condition = 'excellent';
+    } else if (data.condition === 'fair') {
+      condition = 'fair';
+    }
+    console.log(`  Adding stuff: ${data.name}(${data.owner})`);
+
+    // You must provide a unique identifier for the 'where' clause, such as 'id'
+    // If you don't have an id, you may need to query for it first or change your schema to make 'name' unique
+    // Here is an example assuming you have 'id' in your data:
+    // You must provide a unique identifier for the 'where' clause, such as 'id'
+    // If you don't have an id, you may need to query for it first or change your schema to make 'name' unique
+    // Here is an example assuming you have 'id' in your data:
+    // First, try to find the existing stuff by name and owner
+    const existingStuff = await prisma.stuff.findFirst({
+      where: {
+        name: data.name,
+        owner: data.owner,
+      },
+    });
+
+    if (existingStuff) {
+      await prisma.stuff.update({
+        where: { id: existingStuff.id },
+        data: {
+          quantity: data.quantity,
+          condition,
+        },
+      });
+    } else {
+      await prisma.stuff.create({
+        data: {
           name: data.name,
           quantity: data.quantity,
           owner: data.owner,
           condition,
         },
-      }),
-    );
-  }
-  await Promise.all(stuffPromises);
-
-  const contactPromises = [];
-  for (const contact of config.defaultContacts) {
+      });
+    }
+  });
+  config.defaultContacts.forEach(async (contact) => {
     console.log(`  Adding contact: ${contact.firstName} ${contact.lastName}`);
-    contactPromises.push(
-      prisma.contact.upsert({
-        where: {
-          firstName_lastName: {
-            firstName: contact.firstName,
-            lastName: contact.lastName,
-          },
-        },
-        update: {},
-        create: {
+    await prisma.contact.upsert({
+      where: {
+        firstName_lastName: {
           firstName: contact.firstName,
           lastName: contact.lastName,
-          address: contact.address,
-          image: contact.image,
-          description: contact.description,
-          owner: contact.owner,
         },
-      }),
-    );
-  }
-  await Promise.all(contactPromises);
+      },
+      update: {},
+      create: {
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        address: contact.address,
+        description: contact.description,
+        image: contact.image,
+        owner: contact.owner,
+      },
+    });
+  });
 }
 
 main()
--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { Contact } from '@prisma/client';
-import { Card } from 'react-bootstrap';
+import { Card, Image } from 'react-bootstrap';
 
 /* Renders a single Contact. See list/page.tsx. */
 const ContactCardAdmin = ({ contact }: { contact : Contact }) => (
   <Card className="h-100">
     <Card.Header>
-      <img src={contact.image} width={75} alt={`${contact.firstName} ${contact.lastName}`} />
+      <Image src={contact.image} width={75} />
       <Card.Title>
         {contact.firstName}
         &nbsp;
@@ -16,13 +16,11 @@ const ContactCardAdmin = ({ contact }: { contact : Contact }) => (
       <Card.Subtitle>{contact.address}</Card.Subtitle>
     </Card.Header>
     <Card.Body>
-      <Card.Text>
-        {contact.description}
-        <p className="block-quotefooter">
-          {contact.owner}
-        </p>
-      </Card.Text>
+      <Card.Text>{contact.description}</Card.Text>
     </Card.Body>
+    <Card.Footer className="block-quote-footer">
+      {contact.owner}
+    </Card.Footer>
   </Card>
 );
 
