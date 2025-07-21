58ab3f6e6f1d5fb1b7167cce89452133a0336d1f
Chihiro Oki <oc9@hawaii.edu>
2025-07-20 22:42:21 -1000

    clean up

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,12 +1,5 @@
 import * as Yup from 'yup';
 
-export const AddStuffSchema = Yup.object({
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
 export const AddContactSchema = Yup.object({
   firstName: Yup.string().required(),
   lastName: Yup.string().required(),
@@ -29,13 +22,5 @@ export const EditContactSchema = Yup.object({
   address: Yup.string().required(),
   description: Yup.string().required(),
   image: Yup.string().url().required(),
-  owner: Yup.string().required(),
-});
-
-export const EditStuffSchema = Yup.object({
-  id: Yup.number().required(),
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
   owner: Yup.string().required(),
 });
